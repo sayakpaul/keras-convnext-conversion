ca4fcc1f5a85cd71fc30d2e9ef7d59a6410e4aaa
Sayak Paul <spsayakpaul@gmail.com>
2022-04-17 11:40:37 +0530

    chore: removed unnecessary file.

--- test_model_init.py
+++
@@ -1,6 +0,0 @@
-import convnext
-
-convnext_tiny = convnext.ConvNeXt(
-  **convnext.MODEL_CONFIGS.get("tiny"), drop_path_rate=0.0, layer_scale_init_value=0.0,  model_name="convnext_tiny"
-)
-print(convnext_tiny.summary(expand_nested=True))
